80a1b44048346aafbf7aa46e8fd2fe897da1ef82
Oswaldo <U22209742@utp.edu.pe>
2025-06-22 11:30:42 -0500

    feat:Creacion de ProductoController y configuracion de puerto

---
+++ src/main/java/com/herramientas/microserviceproducto/controller/ProductoController.java
@@ -0,0 +1,47 @@
+package com.herramientas.microserviceproducto.controller;
+
+import com.herramientas.microserviceproducto.entity.Producto;
+import com.herramientas.microserviceproducto.service.IProductoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/producto")
+public class ProductoController {
+
+    private final IProductoService productoService;
+
+    public ProductoController(IProductoService productoService){
+        this.productoService=productoService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Producto> getProductById(@PathVariable Long id){
+        return ResponseEntity.ok(productoService.getProductById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Producto>> getAllProducts(){
+        return ResponseEntity.ok(productoService.getAllProducts());
+    }
+
+    @PostMapping
+    public ResponseEntity<Producto> createProduct(@RequestBody Producto producto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(productoService.createProduct(producto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Producto> updateProduct(@PathVariable Long id,@RequestBody Producto producto){
+        return ResponseEntity.ok(productoService.updateProduct(id,producto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        productoService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
