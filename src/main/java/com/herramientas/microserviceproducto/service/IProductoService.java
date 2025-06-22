43e6ebfed1e84131fec6e99ecde998181f452358
Oswaldo <U22209742@utp.edu.pe>
2025-06-22 11:17:19 -0500

    feat:Desarrollo de IProductoService

--- src/main/java/com/herramientas/microserviceproducto/service/IProductoService.java
+++ src/main/java/com/herramientas/microserviceproducto/service/IProductoService.java
@@ -1,4 +1,13 @@
 package com.herramientas.microserviceproducto.service;
 
+import com.herramientas.microserviceproducto.entity.Producto;
+
+import java.util.List;
+
 public interface IProductoService {
+    Producto getProductById(Long id);
+    List<Producto> getAllProducts();
+    Producto createProduct(Producto producto);
+    Producto updateProduct(Long id, Producto producto);
+    void deleteProduct(Long id);
 }
