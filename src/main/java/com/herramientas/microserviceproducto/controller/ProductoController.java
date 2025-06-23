2ba48e8365311e26e6efbb8c632e818e9a7e0745
Oswaldo <U22209742@utp.edu.pe>
2025-06-22 22:54:57 -0500

    fix:cambio en la ruta

--- src/main/java/com/herramientas/microserviceproducto/controller/ProductoController.java
+++ src/main/java/com/herramientas/microserviceproducto/controller/ProductoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/producto")
+@RequestMapping("/producto")
 public class ProductoController {
 
     private final IProductoService productoService;
